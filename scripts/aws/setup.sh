7be9a3e6454d8998e9bf8190eb31631d2188f0f4
Simon Ochsenreither <s8siochs@stud.uni-saarland.de>
2023-07-06 22:37:59 +0200

    fix for aws setup

--- scripts/aws/setup.sh
+++ scripts/aws/setup.sh
@@ -11,7 +11,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > install-rust.sh
 bash install-rust.sh -y
 source $HOME/.cargo/env
 
-git clone https://github.com/zhtluo/randpiper-rs.git
+git clone https://github.com/sochsenreither/randpiper-rs.git
 cd randpiper-rs
 
 git pull
